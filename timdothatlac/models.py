7029d67c158f23bc450bd6ca99afdf6075e9d650
matthew28302 <20521508@gm.uit.edu.vn>
2022-10-05 14:43:16 +0700

    new data

--- timdothatlac/models.py
+++ timdothatlac/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    username = models.CharField(("Username"), max_length=50)
-    password = models.CharField(("Password"), max_length=50)    
+    username = models.CharField(("Tên đăng nhập"), max_length=50)
+    password = models.CharField(("Mật khẩu"), max_length=50)  
+class Information(models.Model):
+    name = models.CharField(("Họ tên"), max_length=50)
+    address = models.CharField(("Địa chỉ"), max_length=50)
+    phoneNumber = models.PhoneNumberField(("Số điện thoại"))
+class Item:
+    typeItem = models.CharField(("Loại đồ"), max_length=50)
+    catalog = models.CharField(("Danh sách kiểu đồ"), max_length=50)
+    time = models.TimeField(("Thời gian"), auto_now=False, auto_now_add=False)
+    address = models.CharField(("Địa điểm"), max_length=50)
+    content = models.CharField(("Nội dung"), max_length=500)
+    image = models.ImageField((""), upload_to=None, height_field=None, width_field=None, max_length=None)
+    status = models.CharField(("Tình trạng"), max_length=50)
 
     
